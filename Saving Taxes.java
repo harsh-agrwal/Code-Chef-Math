3355098a353d54b20dd4963a353a5d31e1969673
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-04-16 22:34:48 +0530

    Create Saving Taxes.java

---
+++ Saving Taxes.java
@@ -0,0 +1,23 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int minInvest = Math.max(0, x - y);
+            System.out.println(minInvest);
+        }
+        sc.close();
+		// your code goes here
+	}
+}
