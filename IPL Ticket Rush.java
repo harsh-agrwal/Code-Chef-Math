06eaa17e36b6c6459535f0e960ab45768450ea94
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-04-21 23:15:11 +0530

    Create IPL Ticket Rush.java

---
+++ IPL Ticket Rush.java
@@ -0,0 +1,27 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int diff = n - m;
+            if (diff > 0) {
+                System.out.println(diff);
+            } else {
+                System.out.println(0);
+            }
+        }
+        sc.close();
+		// your code goes here
+	}
+}
