76ebc852a3f014c55a3d77edefad4742510f7562
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-04-27 16:42:59 +0530

    Create Reach the Target.java

---
+++ Reach the Target.java
@@ -0,0 +1,21 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+			Scanner in=new Scanner(System.in);
+		int T=in.nextInt();
+		while(T-->0){
+		    int x=in.nextInt();
+		    int y=in.nextInt();
+		    int dif=x-y;
+		    System.out.println(dif);
+		}// your code goes here
+	}
+}
