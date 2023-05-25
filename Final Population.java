21f7f4eb71c4de0c0335ea5a9851c8c8a8c14ac8
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-05-25 11:49:56 +0530

    Create Final Population.java

---
+++ Final Population.java
@@ -0,0 +1,22 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc =new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		    int z=sc.nextInt();
+		    System.out.println((x-y)+z);
+		}	// your code goes here
+	}
+}
