1aa743b84c2ca4d6f241d85f8488ab7cd37745da
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-05-27 11:06:43 +0530

    Create Ticket Fine.java

---
+++ Ticket Fine.java
@@ -0,0 +1,20 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    int x=sc.nextInt(),p=sc.nextInt(),q=sc.nextInt();
+		    System.out.println((p-q)*x);
+		}	// your code goes here
+	}
+}
