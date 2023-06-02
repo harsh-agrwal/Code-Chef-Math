4282367f9e4837b3f5ee6e56ff2c57f8411cb333
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-06-02 21:49:50 +0530

    Create Car Trip.java

---
+++ Car Trip.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner s=new Scanner(System.in);
+	    int n=s.nextInt();
+	    while(n>0){
+	        int a=s.nextInt();
+	        if(a<=300)
+	        System.out.println("3000");
+	        else
+	        System.out.println(a*10);
+	        n--;
+	        
+	        
+	    }
+		// your code goes here
+	}
+}
