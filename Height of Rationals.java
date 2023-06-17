fc27e5e7a95066434da9d9eeb57f77c76016b6ec
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-06-17 20:59:44 +0530

    Create Height of Rationals.java

---
+++ Height of Rationals.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class A{
+	public static void main (String[] args){
+		Scanner sc=new Scanner(System.in);
+		int t,a,b,e;
+		a=sc.nextInt();
+		b=sc.nextInt();
+		int c;
+		c=Math.max(a,b);
+		System.out.println(c);
+	}
+}
