3922c32f922abcac5623ddbdc1b4238508be9d19
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-06-23 15:30:49 +0530

    Create Maximum Submissions.java

---
+++ Maximum Submissions.java
@@ -0,0 +1,22 @@
+// We have populated the solutions for the 10 easiest problems for your support.
+// Click on the SUBMIT button to make a submission to this problem.
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	int t=sc.nextInt();
+	for(int i=0;i<t;i++)
+	{
+	    int x=sc.nextInt();
+	      System.out.println(x*2);
+	      
+	  }
+	}
+}
+
