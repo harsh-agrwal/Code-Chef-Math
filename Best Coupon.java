f4e26f5964f4ff44ac1e246c51d529bcbdd734f6
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-07-24 15:32:30 +0530

    Create Best Coupon.java

---
+++ Best Coupon.java
@@ -0,0 +1,23 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t, x;
+		
+		t = sc.nextInt();
+		while (t --> 0){
+		    x = sc.nextInt();
+		    double c = x * 0.1;
+		    System.out.println((int)Math.max(c, 100));
+		}
+	}
+}
