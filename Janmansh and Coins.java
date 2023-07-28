8a70087d2629d53549687d0f3e4e9c5b2ec5fa65
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-07-28 13:19:43 +0530

    Create Janmansh and Coins.java

---
+++ Janmansh and Coins.java
@@ -0,0 +1,19 @@
+/* package codechef; // don't place package name! */
+
+import java.util.Scanner;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner in = new Scanner(System.in);
+		int T = in.nextInt(); 
+		while(T-->0) {
+		    int x = in.nextInt(); 
+		    int y = in.nextInt(); 
+		    System.out.println(x*10 + y*5);
+		}
+	}
+}
