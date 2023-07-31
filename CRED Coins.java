e71911e0ca07c830ff2f2891f249617ee5b7d94d
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-07-31 08:58:48 +0530

    Create CRED Coins.java

---
+++ CRED Coins.java
@@ -0,0 +1,22 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner s=new Scanner(System.in);
+	    int n=s.nextInt();
+	    for(int i=0;i<n;i++){
+	        int a=s.nextInt();
+	        int b=s.nextInt();
+	        int c=a*b;
+	        System.out.println(c/100);
+	    }
+		// your code goes here
+	}
+}
